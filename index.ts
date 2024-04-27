f9e5cb93b7012c9814ff7e5402432ab8f9278e6f
tiavina-mika <tiavinamika@gmail.com>
2024-04-27 07:04:23 +0300

    Update index.ts

--- index.ts
+++ index.ts
@@ -1,6 +1,7 @@
 import TextEditor from './src/components/TextEditor';
-export { type TextEditorProps } from './src/components/TextEditor';
 import TextEditorReadOnlyComponent from './src/components/TextEditorReadOnly';
+
+export { type TextEditorProps } from './src/components/TextEditor';
 
 export const TextEditorReadOnly = TextEditorReadOnlyComponent;
 export default TextEditor;
