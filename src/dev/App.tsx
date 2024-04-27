c0820610ff24ba4026bcbd2ee5d56077692c021f
tiavina-mika <tiavinamika@gmail.com>
2024-04-27 10:05:22 +0300

    Create App.tsx

---
+++ src/dev/App.tsx
@@ -0,0 +1,26 @@
+import React from 'react'
+import { ThemeProvider as EmotionThemeProvider } from '@emotion/react';
+import { ThemeProvider } from '@mui/material';
+import TextEditor from '../components/TextEditor';
+
+import { getTheme } from './theme.utils';
+
+const App = () => {
+  const theme = getTheme();
+
+  return (
+    <ThemeProvider theme={theme}>
+      <EmotionThemeProvider theme={theme}>
+        <TextEditor
+          className="flexColumn stretchSelf flex1"
+          label="Description"
+          placeholder="Provide as much information as possible. This field has only one limit, yours."
+          // mentions={mentions}
+          // menuClassName={classes.textEditorMenu}
+        />
+      </EmotionThemeProvider>
+    </ThemeProvider>
+  )
+}
+
+export default App
