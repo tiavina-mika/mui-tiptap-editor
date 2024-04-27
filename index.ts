e2b90a225e5c21aad84f3f66c5c1f3cd824bd5af
tiavina-mika <tiavinamika@gmail.com>
2024-04-27 10:04:50 +0300

    Delete index.ts

--- index.ts
+++
@@ -1,7 +0,0 @@
-export {
-  default as TextEditor,
-  type TextEditorProps,
-} from './src/components/TextEditor';
-export {
-  default as TextEditorReadOnly,
-} from './src/components/TextEditorReadOnly';
