53707b9f5308b1132383048a1f53303d50a61536
tiavina-mika <tiavinamika@gmail.com>
2024-04-27 10:05:46 +0300

    Create type.d.ts

---
+++ src/type.d.ts
@@ -0,0 +1,33 @@
+import { ReactNode } from "react";
+
+export interface ISelectOption<T = string> {
+  label: string;
+  value: T,
+  icon?: string | ReactNode;
+  hide?: boolean;
+}
+
+
+export enum EditorToolbarEnum {
+  heading = 'heading',
+  bold = 'bold',
+  italic = 'italic',
+  strike = 'strike',
+  link = 'link',
+  underline = 'underline',
+  image = 'image',
+  code = 'code',
+  orderedList = 'orderedList',
+  bulletList = 'bulletList',
+  align = 'align',
+  codeBlock = 'codeBlock',
+  blockquote = 'blockquote',
+  table = 'table',
+  history = 'history',
+  youtube = 'youtube',
+  color = 'color',
+  mention = 'mention',
+  ai = 'ai'
+}
+
+export type IEditorToolbar = `${EditorToolbarEnum}`;
