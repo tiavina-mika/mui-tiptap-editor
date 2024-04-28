76d3799efa9a37f59c6a1e50cea1eb4a9775fd92
tiavina-mika <tiavinamika@gmail.com>
2024-04-28 12:58:48 +0300

    Create App.tsx

---
+++ example/src/App.tsx
@@ -0,0 +1,13 @@
+import MuiThemeProvider from './MuiThemeProvider';
+
+import Form from './Form';
+
+const App = () => {
+  return (
+    <MuiThemeProvider>
+      <Form />
+    </MuiThemeProvider>
+  )
+}
+
+export default App
