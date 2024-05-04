0b7d00fc985d340d682ee9380c27f4e0ee02aed8
tiavina-mika <tiavinamika@gmail.com>
2024-05-04 19:13:21 +0300

    feat: add icon container

---
+++ src/icons/Icon.tsx
@@ -0,0 +1,16 @@
+import { ReactNode } from "react";
+
+type Props = {
+  children: ReactNode;
+  size?: number;
+};
+
+const Icon = ({ children, size = 18 }: Props) => {
+  return (
+    <div css={{ '& svg': { fontSize: size }}}>
+      {children}
+    </div>
+  )
+}
+
+export default Icon;
