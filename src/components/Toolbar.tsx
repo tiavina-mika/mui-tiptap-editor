be6d19988df85128d29f9a3dc793b6f8922b827f
tiavina-mika <tiavinamika@gmail.com>
2024-05-25 13:59:07 +0300

    Update Toolbar.tsx

--- src/components/Toolbar.tsx
+++ src/components/Toolbar.tsx
@@ -67,13 +67,13 @@ export type ToolbarProps = {
   className?: string;
     /**
    * toolbar (each icon) to be displayed
-   * 
+   *
    * possible values are: [
    * "heading", "bold", "italic", "strike", "link", "underline", "image", "code",
    * "orderedList", "bulletList", "align", "codeBlock", "blockquote", "table",
    * "history", "youtube", "color", "mention"
    * ]
-   * 
+   *
    * default values is all the above
    */
   toolbar?: IEditorToolbar[];
@@ -102,154 +102,165 @@ const Toolbar = ({
     setTableAnchorEl(null);
   };
 
-  const menus = useMemo(() => [
-    {
-      name: "bold",
-      icon: Bold,
-      iconSize: 12,
-      onClick: () => editor.chain().focus().toggleBold().run(),
-      disabled: !editor.can().chain().focus().toggleBold().run(),
-      tooltip: 'Bold'
-    },
-    {
-      name: "italic",
-      icon: Italic,
-      iconSize: 12,
-      onClick: () => editor.chain().focus().toggleItalic().run(),
-      disabled: !editor.can().chain().focus().toggleItalic().run(),
-      tooltip: 'Italic'
-    },
-    {
-      name: "strike",
-      icon: Strike,
-      iconSize: 14,
-      onClick: () => editor.chain().focus().toggleStrike().run(),
-      disabled: !editor.can().chain().focus().toggleStrike().run(),
-      tooltip: 'Strike through'
-    },
-    {
-      name: "underline",
-      iconSize: 14,
-      icon: Underline,
-      onClick: () => editor.chain().focus().toggleUnderline().run(),
-      disabled: !editor.can().chain().focus().toggleUnderline().run(),
-      tooltip: 'Underline'
-    },
-    {
-      name: "link",
-      icon: Link,
-      onClick: toggleLinkDialog,
-      disabled: false,
-      split: true,
-      tooltip: 'Link'
-    },
-    // order
-    {
-      name: "bulletList",
-      icon: BulletList,
-      onClick: () => editor.chain().focus().toggleBulletList().run(),
-      disabled: !editor.can().chain().focus().toggleBulletList().run(),
-      tooltip: 'Bullet list'
-    },
-    {
-      name: "orderedList",
-      icon: OrderedList,
-      onClick: () => editor.chain().focus().toggleOrderedList().run(),
-      disabled: !editor.can().chain().focus().toggleOrderedList().run(),
-      split: true,
-      tooltip: 'Ordered list',
-      iconSize: 14
-    },
-    // alignment
-    {
-      name: "align-left",
-      icon: AlignLeft,
-      onClick: () => editor.chain().focus().setTextAlign("left").run(),
-      disabled: false,
-      active: { textAlign: "left" },
-      group: "align",
-      tooltip: 'Left align'
-    },
-    {
-      name: "align-center",
-      icon: AlignCenter,
-      onClick: () => editor.chain().focus().setTextAlign("center").run(),
-      disabled: false,
-      active: { textAlign: "center" },
-      group: "align",
-      tooltip: 'Center align'
-    },
-    {
-      name: "align-right",
-      icon: AlignRight,
-      onClick: () => editor.chain().focus().setTextAlign("right").run(),
-      disabled: false,
-      active: { textAlign: "right" },
-      group: "align",
-      tooltip: 'Right align'
-    },
-    {
-      name: "align-justify",
-      icon: AlignJustify,
-      onClick: () => editor.chain().focus().setTextAlign("justify").run(),
-      disabled: false,
-      active: { textAlign: "justify" },
-      split: true,
-      group: "align",
-      tooltip: 'Justify align'
-    },
-    {
-      name: "blockquote",
-      icon: Quote,
-      onClick: () => editor.chain().focus().toggleBlockquote().run(),
-      disabled: false,
-      tooltip: 'Block quote',
-      iconSize: 16
-    },
-    {
-      name: "codeBlock",
-      icon: Code,
-      onClick: () => editor.chain().focus().toggleCodeBlock().run(),
-      disabled: false,
-      split: true,
-      tooltip: 'Code block'
-    },
-    {
-      name: "table",
-      icon: Table,
-      onClick: (event: MouseEvent<HTMLElement>) => {
-        handleOpenTableMenu(event);
+  const menus = useMemo(() => {
+    return [
+      {
+        name: "bold",
+        icon: Bold,
+        iconSize: 12,
+        onClick: () => editor.chain().focus().toggleBold().run(),
+        disabled: !editor.can().chain().focus().toggleBold().run(),
+        tooltip: 'Bold'
       },
-      disabled: false,
-      split: true,
-      tooltip: 'Table'
-    },
-    {
-      name: "youtube",
-      icon: Youtube,
-      onClick: toggleYoutubeDialog,
-      disabled: false,
-      split: true,
-      tooltip: 'Youtube'
-    },
-    {
-      name: "undo",
-      icon: Undo,
-      onClick: () => editor.chain().focus().undo().run(),
-      disabled: !editor.can().undo(),
-      default: true, // always display
-      tooltip: 'Undo'
-    },
-    {
-      name: "redo",
-      icon: Redo,
-      onClick: () => editor.chain().focus().redo().run(),
-      disabled: !editor.can().redo(),
-      default: true, // always display
-      split: true,
-      tooltip: 'Redo'
-    }
-  ], [editor, toggleLinkDialog, toggleYoutubeDialog, handleOpenTableMenu]);
+      {
+        name: "italic",
+        icon: Italic,
+        iconSize: 12,
+        onClick: () => editor.chain().focus().toggleItalic().run(),
+        disabled: !editor.can().chain().focus().toggleItalic().run(),
+        tooltip: 'Italic'
+      },
+      {
+        name: "strike",
+        icon: Strike,
+        iconSize: 14,
+        onClick: () => editor.chain().focus().toggleStrike().run(),
+        disabled: !editor.can().chain().focus().toggleStrike().run(),
+        tooltip: 'Strike through'
+      },
+      {
+        name: "underline",
+        iconSize: 14,
+        icon: Underline,
+        onClick: () => editor.chain().focus().toggleUnderline().run(),
+        disabled: !editor.can().chain().focus().toggleUnderline().run(),
+        tooltip: 'Underline'
+      },
+      {
+        name: "link",
+        icon: Link,
+        onClick: toggleLinkDialog,
+        disabled: false,
+        split: true,
+        tooltip: 'Link'
+      },
+      // order
+      {
+        name: "bulletList",
+        icon: BulletList,
+        onClick: () => editor.chain().focus().toggleBulletList().run(),
+        disabled: !editor.can().chain().focus().toggleBulletList().run(),
+        tooltip: 'Bullet list'
+      },
+      {
+        name: "orderedList",
+        icon: OrderedList,
+        onClick: () => editor.chain().focus().toggleOrderedList().run(),
+        disabled: !editor.can().chain().focus().toggleOrderedList().run(),
+        split: true,
+        tooltip: 'Ordered list',
+        iconSize: 14
+      },
+      // alignment
+      {
+        name: "align-left",
+        icon: AlignLeft,
+        onClick: () => editor.chain().focus().setTextAlign("left").run(),
+        disabled: false,
+        active: { textAlign: "left" },
+        group: "align",
+        tooltip: 'Left align'
+      },
+      {
+        name: "align-center",
+        icon: AlignCenter,
+        onClick: () => editor.chain().focus().setTextAlign("center").run(),
+        disabled: false,
+        active: { textAlign: "center" },
+        group: "align",
+        tooltip: 'Center align'
+      },
+      {
+        name: "align-right",
+        icon: AlignRight,
+        onClick: () => editor.chain().focus().setTextAlign("right").run(),
+        disabled: false,
+        active: { textAlign: "right" },
+        group: "align",
+        tooltip: 'Right align'
+      },
+      {
+        name: "align-justify",
+        icon: AlignJustify,
+        onClick: () => editor.chain().focus().setTextAlign("justify").run(),
+        disabled: false,
+        active: { textAlign: "justify" },
+        split: true,
+        group: "align",
+        tooltip: 'Justify align'
+      },
+      {
+        name: "blockquote",
+        icon: Quote,
+        onClick: () => editor.chain().focus().toggleBlockquote().run(),
+        disabled: false,
+        tooltip: 'Block quote',
+        iconSize: 16
+      },
+      {
+        name: "codeBlock",
+        icon: Code,
+        onClick: () => editor.chain().focus().toggleCodeBlock().run(),
+        disabled: false,
+        split: true,
+        tooltip: 'Code block'
+      },
+      {
+        name: "table",
+        icon: Table,
+        onClick: (event: MouseEvent<HTMLElement>) => {
+          handleOpenTableMenu(event);
+        },
+        disabled: false,
+        split: true,
+        tooltip: 'Table'
+      },
+      {
+        name: "youtube",
+        icon: Youtube,
+        onClick: toggleYoutubeDialog,
+        disabled: false,
+        split: true,
+        tooltip: 'Youtube'
+      },
+      {
+        name: "undo",
+        icon: Undo,
+        onClick: () => editor.chain().focus().undo().run(),
+        disabled: !editor.can().undo(),
+        default: true, // always display
+        tooltip: 'Undo'
+      },
+      {
+        name: "redo",
+        icon: Redo,
+        onClick: () => editor.chain().focus().redo().run(),
+        disabled: !editor.can().redo(),
+        default: true, // always display
+        split: true,
+        tooltip: 'Redo'
+      }
+    ]
+  }, [
+    editor,
+    // for some unknown reason, the history is not being updated
+    // so we need to force the update
+    editor.can().undo,
+    editor.can().redo,
+    toggleLinkDialog,
+    toggleYoutubeDialog,
+    handleOpenTableMenu
+  ]);
 
   return (
     <div className={cx(className, 'flexRow center')} css={classes.toolbar}>
