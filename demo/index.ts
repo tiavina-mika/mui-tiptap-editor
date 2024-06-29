f0c435532856676746874bc1f7b7f3a49d74aded
tiavina-mika <tiavinamika@gmail.com>
2024-06-29 17:01:51 +0300

    Create index.ts

---
+++ demo/index.ts
@@ -0,0 +1,14 @@
+export {
+  default as TextEditor,
+} from './components/TextEditor';
+export {
+  default as TextEditorReadOnly,
+} from './components/TextEditorReadOnly';
+export {
+  default as Toolbar,
+  type ToolbarProps,
+} from './components/Toolbar';
+export {
+  type TextEditorProps,
+  type ITextEditorOption
+} from './types.d';
