ee64ef7aea5a51f85341be07c0fcda42c79c1f83
tiavina-mika <tiavinamika@gmail.com>
2024-06-29 17:00:14 +0300

    Create TextEditorReadOnly.tsx

---
+++ demo/components/TextEditorReadOnly.tsx
@@ -0,0 +1,31 @@
+import "../index.css";
+
+import {
+  EditorContent,
+} from '@tiptap/react';
+
+import { useTextEditor } from "../hooks/useTextEditor";
+
+type TextEditorProps = {
+  className?: string;
+  value?: string;
+  tab?: 'editor' | 'preview';
+};
+
+const TextEditorReadOnly = ({
+  value,
+  className,
+  tab = 'preview',
+  ...editorOptions
+}: TextEditorProps) => {
+  const editor = useTextEditor({
+    value,
+    tab,
+    editable: false,
+    ...editorOptions
+  })
+
+  return <EditorContent editor={editor} className={className} />;
+};
+
+export default TextEditorReadOnly;
