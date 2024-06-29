bd4ac370189e8077eab3dc1ec6003a03d59a6a5f
tiavina-mika <tiavinamika@gmail.com>
2024-06-29 17:01:21 +0300

    Create Redo.tsx

---
+++ demo/icons/Redo.tsx
@@ -0,0 +1,14 @@
+import { SvgIcon } from "@mui/material";
+
+const Redo = () => {
+    return (
+      <SvgIcon>
+        <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round" className="feather feather-corner-up-right">
+          <polyline points="15 14 20 9 15 4" />
+          <path d="M4 20v-7a4 4 0 0 1 4-4h12" />
+        </svg>
+      </SvgIcon>
+    );
+}
+
+export default Redo;
