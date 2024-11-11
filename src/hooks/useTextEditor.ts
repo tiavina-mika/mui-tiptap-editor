7e2e78406880702d518b60cb9a3ba22a037b2d15
tiavina-mika <tiavinamika@gmail.com>
2024-11-11 21:42:02 +0300

    remove "duplicated" warning

--- src/hooks/useTextEditor.ts
+++ src/hooks/useTextEditor.ts
@@ -70,8 +70,13 @@ const extensions = [
       keepMarks: true,
       keepAttributes: false, // TODO : Making this as `false` becase marks are not preserved when I try to preserve attrs, awaiting a bit of help
     },
-    // history: false, // important because history will now be handled by Y.js
+    // disable the following nodes, since we already have our own
     codeBlock: false,
+    paragraph: false,
+    document: false,
+    gapcursor: false,
+    heading: false,
+    text: false,
   }),
   Heading.configure({
     HTMLAttributes: {
