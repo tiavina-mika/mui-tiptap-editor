5cb327dc40c01377c6fee1e29ebd15d7b5496ead
tiavina-mika <tiavinamika@gmail.com>
2025-10-18 17:31:43 +0300

    config prettier

---
+++ .prettierrc.mjs
@@ -0,0 +1,12 @@
+export default {
+  arrowParens: 'always',
+  quoteProps: 'preserve',
+  bracketSameLine: false,
+  endOfLine: 'lf',
+  printWidth: 80,
+  semi: true,
+  singleQuote: true,
+  tabWidth: 2,
+  trailingComma: 'all',
+  useTabs: false,
+};
