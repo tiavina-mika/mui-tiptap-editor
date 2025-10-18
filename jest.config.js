2cce282e19cde3f3fa2574c847c832e00dc02f52
tiavina-mika <tiavinamika@gmail.com>
2025-10-18 17:37:27 +0300

    Delete jest.config.js

--- jest.config.js
+++
@@ -1,8 +0,0 @@
-/** @type {import('ts-jest').JestConfigWithTsJest} */
-export default {
-  preset: 'ts-jest',
-  testEnvironment: 'node',
-  testMatch: ['**/__tests__/?(*.)+(spec|test).ts?(x)'],
-  moduleFileExtensions: ['ts', 'tsx', 'js', 'jsx', 'json', 'node'],
-  verbose: true
-};
