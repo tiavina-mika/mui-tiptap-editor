9288910d2008256c66ca77396e1d0bd5b0de4284
tiavina-mika <tiavinamika@gmail.com>
2025-10-18 17:39:06 +0300

    Update index.ts

--- src/index.ts
+++ src/index.ts
@@ -1,14 +1,4 @@
-export {
-  default as TextEditor,
-} from './components/TextEditor';
-export {
-  default as TextEditorReadOnly,
-} from './components/TextEditorReadOnly';
-export {
-  default as Toolbar,
-  type ToolbarProps,
-} from './components/Toolbar';
-export {
-  type TextEditorProps,
-  type ITextEditorOption,
-} from './types.d';
+export { default as TextEditor } from './pages/TextEditor';
+export { default as TextEditorReadOnly } from './pages/TextEditorReadOnly';
+export { default as Toolbar, type ToolbarProps } from './components/Toolbar';
+export { type TextEditorProps, type ITextEditorOption } from './types';
