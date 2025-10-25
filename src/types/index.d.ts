10f45c905625152bd795620cac80058fc11f06ae
tiavina-mika <tiavinamika@gmail.com>
2025-10-25 11:14:46 +0300

    rename types import

--- src/types/index.d.ts
+++ src/types/index.d.ts
@@ -1,5 +1,4 @@
-export { type TextEditorProps, type ITextEditorOption } from './text-editor.d';
-export { type ToolbarProps } from './toolbar.d';
-export { default as TextEditor } from './pages/TextEditor.d';
-export { default as TextEditorReadOnly } from './pages/TextEditorReadOnly.d';
-export { default as Toolbar } from './components/Toolbar.d';
+export { type TextEditorProps, type ITextEditorOption } from './text-editor';
+export { type ToolbarProps } from './toolbar';
+export { default as TextEditor } from './pages/TextEditor';
+export { default as TextEditorReadOnly } from './pages/TextEditorReadOnly';
