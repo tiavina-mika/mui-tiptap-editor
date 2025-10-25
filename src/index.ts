fee1367e601c9801d83ea6393dec22028a4b51b6
tiavina-mika <tiavinamika@gmail.com>
2025-10-25 11:12:12 +0300

    fix: type import in entry file

--- src/index.ts
+++ src/index.ts
@@ -1,8 +1,7 @@
-export { default as TextEditor } from './pages/TextEditor';
-export { default as TextEditorReadOnly } from './pages/TextEditorReadOnly';
-export { default as Toolbar } from './components/Toolbar';
-export {
-  type TextEditorProps,
-  type ITextEditorOption,
-  type ToolbarProps,
-} from './types/index.d';
+export { default as TextEditor } from '@/pages/TextEditor';
+export { default as TextEditorReadOnly } from '@/pages/TextEditorReadOnly';
+export type {
+  TextEditorProps,
+  ITextEditorOption,
+  ToolbarProps,
+} from '@/types';
