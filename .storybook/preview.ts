35e5f8a331cf83b6e47993e688a78b490913be03
tiavina-mika <tiavinamika@gmail.com>
2025-10-25 07:54:02 +0300

    Add MUI wrapper to storybook

--- .storybook/preview.ts
+++ .storybook/preview.ts
@@ -1,7 +1,27 @@
+import '@fontsource/roboto/300.css';
+import '@fontsource/roboto/400.css';
+import '@fontsource/roboto/500.css';
+import '@fontsource/roboto/700.css';
+
+import { CssBaseline, ThemeProvider } from '@mui/material';
+import { withThemeFromJSXProvider } from '@storybook/addon-themes';
 import { INITIAL_VIEWPORTS } from 'storybook/viewport';
 
 import type { Preview } from '@storybook/react-vite';
 
+import { lightTheme, darkTheme } from '../dev/theme';
+
+export const decorators = [
+  withThemeFromJSXProvider({
+    themes: {
+      light: lightTheme,
+      dark: darkTheme,
+    },
+    defaultTheme: 'light',
+    Provider: ThemeProvider,
+    GlobalStyles: CssBaseline,
+  }),
+];
 
 const preview: Preview = {
   parameters: {
