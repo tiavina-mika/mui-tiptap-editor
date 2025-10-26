a2b0e819db0c71fa968db06ab4b01cfd8fed48da
tiavina-mika <tiavinamika@gmail.com>
2025-10-26 09:23:43 +0300

    add image attributes types

--- src/types/text-editor.d.ts
+++ src/types/text-editor.d.ts
@@ -1,26 +1,50 @@
-import type { IEditorToolbar } from './toolbar';
+import type { ILabels } from './labels';
+import type { IEditorToolbar, ImageUploadOptions } from './toolbar';
+import type { CSSProperties, ReactNode } from 'react';
 
 type DeepPartial<T> = {
   [P in keyof T]?: T[P] extends object ? DeepPartial<T[P]> : T[P];
 };
 
+export type ImageAttributes = {
+  src: string;
+  alt?: string;
+  title?: string;
+  width?: number;
+  height?: number;
+  style?: CSSProperties;
+};
+
+/**
+ * Option interface for text editor dropdowns, mentions, collaborators, selects, etc.
+ */
 export interface ITextEditorOption<T = string> {
+  /** Label to be displayed */
   label: string;
+  /**
+   * Value of the option
+   * Can be an id, slug, or any unique identifier
+   */
   value: T;
+  /**
+   * Optional icon to be displayed alongside the label
+   * Can be a URL string or a ReactNode
+   */
   icon?: string | ReactNode;
+  /** If true, the option will be hidden from the list */
   hide?: boolean;
 }
 
 export type TextEditorProps = {
   /**
-   * input id
-   * used if using multiple editors on the same page, to identify uniquely each editor
+   * Input id
+   * Used if using multiple editors on the same page, to identify uniquely each editor
    */
   id?: string;
   placeholder?: string;
 
   /**
-   * input label
+   * Input label
    * NOTE: it is placed above the tabs
    */
   label?: ReactNode;
@@ -31,125 +55,129 @@ export type TextEditorProps = {
   error?: string;
 
   /**
-   * value of the editor
-   * it's an html string, eg: <p>hello world</p>
+   * Value of the editor
+   * It is an html string representing the content of the editor
+   * @example '<p>Some text</p>'
    */
   value?: string;
 
   /**
-   * callback function to be called on change
-   * it returns the html string of the editor, eg: <p>hello world</p>
+   * Callback function to be called on change
+   * it returns the html string of the editor
+   * @param value e.g. '<p>Some text</p>'
+   * @return void
    */
   onChange?: (value: string) => void;
 
   /**
-   * input style override
-   * it's the responsible of the whole input grey border and border radius
+   * Input style to override the default styles
+   * It's the responsible of the whole input grey border and border radius
    *
    */
   inputClassName?: string;
 
   /**
-   * toolbar style override,
-   * it's mainly the responsible of the toolbar border top
+   * Toolbar style to override the default styles
+   * It's mainly the responsible of the toolbar border top
    */
   toolbarClassName?: string;
 
   /**
-   * tabs style override
-   * the tabs 'editor' and 'preview' style
+   * Tabs style to override the default styles
+   * The tabs 'editor' and 'preview' style
    */
   tabsClassName?: string;
 
   /**
-   * tab style override
-   * mainly used for the styles of the current selected tab
+   * Tab style to override the default styles
+   * Mainly used for the styles of the current selected tab
    */
   tabClassName?: string;
 
   /**
-   * error message style override
-   * eg: changing the color of the error message
-   * or its font size
+   * Error message style to override the default styles
+   * @example changing the color of the error message or its font size
    */
   errorClassName?: string;
 
   /*
-   * root class name
-   * it's the main class name of the all the editor
-   * label, tabs, input, toolbar, etc
+   * The root class name to override the default styles
+   * Can be used to style different parts of the editor (using descendant selectors)
    */
   rootClassName?: string;
 
   /**
-   * label styles override
-   * eg: changing the color of the label, its font size, color, etc
+   * Label styles to override the default styles
+   * @example changing the color of the label, its font size, color, etc
    */
   labelClassName?: string;
 
   /**
-   * table of contents container class name
+   * Table of contents container class name
+   * @example to set a max height and make it scrollable
    */
   tableOfContentsClassName?: string;
 
   /**
-   * if true, floating menu will be disabled
+   * If true, floating menu will be disabled
    */
   withFloatingMenu?: boolean;
 
   /**
-   * if true, bubble menu will be displayed
+   * If true, bubble menu will be displayed
    */
   withBubbleMenu?: boolean;
 
   /**
-   * toolbar (each icon) to be displayed
+   * Toolbar (each icon) to be displayed
    *
-   * possible values are: [
+   * Possible values are: [
    * "heading", "bold", "italic", "strike", "link", "underline", "image", "code",
    * "orderedList", "bulletList", "align", "codeBlock", "blockquote", "table",
    * "history", "youtube", "color", "mention"
    * ]
    *
-   * default values is all the above
+   * @default all
    */
   toolbar?: IEditorToolbar[];
 
   /**
-   * toolbar (each icon) to be displayed in bubble menu
+   * Toolbar (each icon) to be displayed in bubble menu
    *
    * @default ['bold', 'italic', 'underline', 'link']
    */
   bubbleMenuToolbar?: IEditorToolbar[];
 
   /**
-   * toolbar (each icon) to be displayed in floating menu
+   * Toolbar (each icon) to be displayed in floating menu
    *
    * @default ['bold', 'italic', 'underline', 'link']
    */
   floatingMenuToolbar?: IEditorToolbar[];
 
   /**
-   * user object for collaboration
-   * the current user or selected user used for collaboration
-   *
+   * User object for collaboration
+   * The current user or selected user used for collaboration
+   * Mainly an object from database representing the user
+   * It should be formatted as ITextEditorOption
    * @example { label: 'John Doe', value: 'some_user_id' }
    */
   user?: ITextEditorOption;
 
   /**
-   * list of users used for mentions
-   *
-   * eg. [{ label: 'John Doe', value: 'some_user_id' }, { label: 'James Smith', value: 'some_user_id_2' }, ...]
-   * NOTE: the value should be unique, it's used for profile url
-   * ex: /profile/:id or /profile/:slug, :id or :slug here is the value
+   * List of users used for mentions
+   * Mainly an array of objects from database representing the users
+   * It should be formatted as ITextEditorOption[]
+   * @example [{ label: 'John Doe', value: 'some_user_id' }, { label: 'James Smith', value: 'some_user_id_2' }, ...]
+   * @Note the value should be unique, it's used for profile url
+   * @example /profile/:id or /profile/:slug, :id or :slug here is the value
    */
   mentions?: ITextEditorOption[];
 
   /**
-   * user pathname for the mentioned user
+   * User pathname for the mentioned user
    *
-   * so the final url will be /profile/:id
+   * So the final url will be /profile/:id
    * :id here is the value in mentions array (see above)
    * the final element is something like: <a href="/profile/some_user_id">{mentioned_user}</a>
    * @example /profile
@@ -157,32 +185,32 @@ export type TextEditorProps = {
   userPathname?: string;
 
   /**
-   * override labels
-   * it's useful for i18n or changing the default labels
+   * Override labels, messages, errors
+   * It's useful for i18n or changing the default labels
    */
   labels?: ILabels;
 
   /**
-   * upload file options
+   * Upload file options
    * ex: file size, number of files, allowed mime types, api callback, etc
    */
   uploadFileOptions?: Omit<ImageUploadOptions, 'type'>;
 
   /**
-   * if true, the tabs (editor, preview) will not be displayed
+   * If true, the tabs (editor, preview) will not be displayed
    * @default false
    */
   disableTabs?: boolean;
 
   /**
-   * position of the toolbar
-   * if top, the toolbar will be displayed at the top of the editor
-   * if bottom, the toolbar will be displayed at the bottom of the editor
+   * Position of the toolbar
+   * If top, the toolbar will be displayed at the top of the editor
+   * If bottom, the toolbar will be displayed at the bottom of the editor
    * @default bottom
    */
   toolbarPosition?: 'top' | 'bottom';
   /**
-   * if true, the table of contents will not be displayed
+   * If true, the table of contents will not be displayed
    * @default true
    */
   disableTableOfContents?: boolean;
